3c91b968c0b9767178fd51a8e4819d394e552ef4
Daniel Gal <daniel.gal@sch.bme.hu>
2019-07-23 15:05:47 +0200

    Fixed backend, added Grommet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Grommet } from 'grommet';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Grommet>
         Hello
-      </div>
+      </Grommet>
     );
   }
 }
