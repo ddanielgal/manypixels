d7d4937bf3d3c774070e91bf1f5b876804dc8735
Daniel Gal <daniel.gal@sch.bme.hu>
2019-07-29 13:45:50 +0200

    This uses divs lol

--- frontend/src/containers/CanvasContainer.js
+++ frontend/src/containers/CanvasContainer.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import { Box } from 'grommet'
 
+import Grid from 'pixel-grid-react'
+import './grid.css'
+
 export default class CanvasContainer extends Component {
+  
+  generateGrid(pxNumber = 20) {
+    const cells = []
+    for (let i = 0; i < pxNumber * pxNumber; i++) {
+      cells.push({
+        width: 100 / pxNumber,
+        color: '#ff0000',
+        id: i
+      })
+    }
+    return cells
+  }
+
   render() {
     return (
-      <Box border='all' pad='medium'>
-        Hello in a box
+      <Box border='all' pad='medium' height='medium' width='medium'>
+        <Grid
+          cells={this.generateGrid(50)}
+          onCellEvent={e => { console.log(e) }}
+        />
       </Box>
     )
   }
