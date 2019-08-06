d16a1f6a76419ed2b3977f8a4695587decf14113
Daniel Gal <daniel.gal@sch.bme.hu>
2019-08-06 07:50:09 +0200

    Pixel border fix

--- frontend/src/components/Canvas.js
+++ frontend/src/components/Canvas.js
@@ -3,14 +3,19 @@ import { Box, Button } from 'grommet'
 
 const Canvas = (props) => {
   return (
-    <Box border='all' pad='medium' direction='column' gap='.1em'
+    <Box border='all' pad='medium' direction='column'
       align='center' justify='center'>
       {props.grid.map((row, y) => (
-        <Box key={y} direction='row' gap='.1em'>
+        <Box key={y} direction='row'>
           {row.map((pixel, x) => (
             <Button plain
               key={x}
-              label={<Box background={pixel} style={{ width: '2em', height: '2em' }} />}
+              label={
+                <Box
+                  background={pixel}
+                  style={{ width: '2em', height: '2em' }}
+                  border={{ size: 'xsmall' }}
+                />}
               onClick={() => props.handlePixelClick(x, y)} />
           ))}
         </Box>
