9709e69e235098f7ed99e4461c0b43c8654d8b1e
Daniel Gal <daniel.gal@sch.bme.hu>
2019-08-06 19:25:11 +0200

    Revert "Pixel border fix"
    This reverts commit d16a1f6a76419ed2b3977f8a4695587decf14113.

--- frontend/src/components/Canvas.js
+++ frontend/src/components/Canvas.js
@@ -4,28 +4,18 @@ import { Box, Button, Grid } from 'grommet'
 const Canvas = (props) => {
   const pixelSize = '1em';
   return (
-    <Box align='center' justify='center' fill>
-      <Box style={{ width: '70vw', height: '70vh' }}>
-        <Grid fill
-          columns={{ count: props.x, size: 'auto' }}>
-          {props.grid.map((color, index) => {
-            const y = Math.floor(index / props.x);
-            const x = index - y * props.x;
-            return (
-              <Button plain fill
-                key={index}
-                label={
-                  <Box
-                    background={color}
-                    border={{ size: 'xsmall' }}
-                    fill
-                    pad={{ top: '100%' }}
-                  />}
-                onClick={() => props.handlePixelClick(x, y)} />
-            )
-          })}
-        </Grid>
-      </Box>
+    <Box border='all' pad='medium' direction='column' gap='.1em'
+      align='center' justify='center'>
+      {props.grid.map((row, y) => (
+        <Box key={y} direction='row' gap='.1em'>
+          {row.map((pixel, x) => (
+            <Button plain
+              key={x}
+              label={<Box background={pixel} style={{ width: '2em', height: '2em' }} />}
+              onClick={() => props.handlePixelClick(x, y)} />
+          ))}
+        </Box>
+      ))}
     </Box>
   )
 }
