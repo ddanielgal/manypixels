a86fef081ce885aa32b6466164e2c1c5712362c3
Daniel Gal <daniel.gal@sch.bme.hu>
2019-08-06 09:27:14 +0200

    Fucking disaster

--- frontend/src/components/Canvas.js
+++ frontend/src/components/Canvas.js
@@ -2,26 +2,31 @@ import React from 'react'
 import { Box, Button, Grid } from 'grommet'
 
 const Canvas = (props) => {
+  const pixelSize = '1em';
   return (
-    <Grid fill
-      columns={{ count: props.x, size: 'auto' }}
-      rows={{ count: props.y, size: 'auto' }}>
-      {props.grid.map((color, index) => {
-        const y = Math.floor(index / props.x);
-        const x = index - y * props.x;
-        return (
-          <Button plain fill
-            key={index}
-            label={
-              <Box
-                background={color}
-                border={{ size: 'xsmall' }}
-                fill
-              />}
-            onClick={() => props.handlePixelClick(x, y)} />
-        )
-      })}
-    </Grid>
+    <Box align='center' justify='center' fill>
+      <Box style={{ width: '70vw', height: '70vh' }}>
+        <Grid fill
+          columns={{ count: props.x, size: 'auto' }}>
+          {props.grid.map((color, index) => {
+            const y = Math.floor(index / props.x);
+            const x = index - y * props.x;
+            return (
+              <Button plain fill
+                key={index}
+                label={
+                  <Box
+                    background={color}
+                    border={{ size: 'xsmall' }}
+                    fill
+                    pad={{ top: '100%' }}
+                  />}
+                onClick={() => props.handlePixelClick(x, y)} />
+            )
+          })}
+        </Grid>
+      </Box>
+    </Box>
   )
 }
 
